b961cb48db31da683f8353033ec8dc40cf5abc08
MsPockets <mcnutterika@gmail.com>
2020-03-11 11:42:28 -0400

    Added event listeners to app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+const authEvents = require('./events')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -7,5 +7,9 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-form').on('submit', authEvents.onSignOut)
+
 })
