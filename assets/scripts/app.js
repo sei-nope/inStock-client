23b08fbb7c18939bdc4a2b923b6bebba3dab858f
YunaH <YunaH503@gmail.com>
2020-03-11 11:34:34 -0400

    Added AJAX request in the API
    Modified api.js, app.js, events.js, ui.js
    Added Sign-up, Sign-in, Change-password and Sign-out AJAX calls

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,13 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const authEvents = require('./events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-form').on('submit', authEvents.onSignOut)
 })
---
+++ assets/scripts/ui.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const store = require('./store')
+
+// Sign Up
+const onSignUpSuccess = function (response) {
+  // console.log(response)
+  // $('#message').removeClass()
+  // $('#message').addClass('success-message')
+  // $('#message').text(response.user.email + ' successfully signed up!')
+  $('#sign-up-form').trigger('reset')
+  $('#sign-in-form').trigger('reset')
+}
+
+const onSignUpFailure = function (response) {
+  // console.log(response)
+  // $('#message').removeClass()
+  // $('#message').addClass('failure-message')
+  // $('#message').text('Failed to sign up')
+  $('#sign-up-form').trigger('reset')
+  $('#sign-in-form').trigger('reset')
+}
+
+// Sign In
+const onSignInSuccess = function (response) {
+  // $('#message').removeClass()
+  // $('#message').addClass('success-message')
+  // $('#message').text(response.user.email + ' successfully signed in')
+  $('#sign-in-form').trigger('reset')
+  $('#sign-up-form').trigger('reset')
+  // store.user = response.user
+}
+
+const onSignInFailure = function (response) {
+  // $('#message').removeClass()
+  // $('#message').addClass('failure-message')
+  // $('#message').text('Signed in failed. 😭')
+  $('#sign-in-form').trigger('reset')
+  $('#sign-up-form').trigger('reset')
+}
+
+// Change Password
+const onChangePasswordSuccess = function (response) {
+  // $('#message').removeClass()
+  // $('#message').addClass('success-message')
+  // $('#message').text('Changed Password Succeeded!')
+  $('#change-password-form').trigger('reset')
+}
+
+const onChangePasswordFailure = function (response) {
+  // $('#message').removeClass()
+  // $('#message').addClass('failure-message')
+  // $('#message').text('You can not change your password muahahaha!')
+  $('#change-password-form').trigger('reset')
+}
+
+// Sign Out
+const onSignOutSuccess = function (response) {
+  // $('#message').removeClass()
+  // $('#message').addClass('success-message')
+  // $('#message').text('Sign Out Succeeded!')
+  $('#change-password-form').hide()
+  $('#change-password-form').trigger('reset')
+  $('#sign-out-form').hide()
+  $('#sign-in-form').show()
+  $('#sign-up-form').show()
+}
+
+const onSignOutFailure = function (response) {
+  // $('#message').removeClass()
+  // $('#message').addClass('failure-message')
+  // $('#message').text('Sign Out Failed 😭')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onChangePasswordSuccess,
+  onChangePasswordFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
